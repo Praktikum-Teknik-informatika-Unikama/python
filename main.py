0834de949389bf2df8c12841f794209990a3e568
aufall02 <aufaluzumaki@gmail.com>
2023-11-09 15:05:53 +0700

    tugas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # print(type(123))
 
 # nama = "praktikum"
-# umur = int(input("masukkan umur anda: "))
+umur = int(input("masukkan umur anda: "))
 # umur = input("masukkan umur anda: ")
 
 # print(nama)
@@ -26,4 +26,5 @@ print("nama saya %s" %nama)
 print(f"nama saya {nama}")
 # string formater versi ketiga
 print("nama saya {}".format(nama))
+
 
