6742b911ae05c17121f895bb3fe6dcf174188802
Jon3sjns <silendprojeck@gmail.com>
2023-11-16 11:27:30 +0700

    remove

--- pertemuan 2/file.py
+++
@@ -1,4 +0,0 @@
-namaDepan = "bilhaq"; namaBelakang = "sahatmojo"; programStudi = "Teknik informatika";nomorIndukMahasiswa = "230403010005"; print("Nama depan: ",namaDepan); print("Nama belakang: ",namaBelakang); print("Prodi: ",programStudi); print("NIM: ",nomorIndukMahasiswa)
-
-nama = "Aufal Marom"
-nim = "2134"
