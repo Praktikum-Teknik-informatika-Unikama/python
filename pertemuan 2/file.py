f09dad4cff78d1ab9ff571dcb003782a2d525e89
Jon3sjns <silendprojeck@gmail.com>
2023-11-16 11:25:08 +0700

    Tugas

---
+++ pertemuan 2/file.py
@@ -0,0 +1,4 @@
+namaDepan = "bilhaq"; namaBelakang = "sahatmojo"; programStudi = "Teknik informatika";nomorIndukMahasiswa = "230403010005"; print("Nama depan: ",namaDepan); print("Nama belakang: ",namaBelakang); print("Prodi: ",programStudi); print("NIM: ",nomorIndukMahasiswa)
+
+nama = "Aufal Marom"
+nim = "2134"
