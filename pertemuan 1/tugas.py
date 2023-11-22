5090f9edcd085cbce033e97628da813e5223a8cf
aufall02 <aufaluzumaki@gmail.com>
2023-11-22 18:01:53 +0700

    update

--- pertemuan 1/tugas.py
+++ pertemuan 1/tugas.py
@@ -1,19 +1,5 @@
-"""
-buat inputan :
-nama
-nim
-prodi
-lalu tampilkan hasil inputan dengan menerapkan string formater versi 3
-"""
+nama_depan = 'aufal'
+nama_belakang = 'marom'
+full_name = nama_depan , nama_belakang
 
-# membuat variable untuk menampung inputan
-nama = input("masukkan nama anda : ")
-nim = input("masukkan nim anda : ")
-prodi = input("masukkan program studi anda : ")
-
-# print inputan yang sudah kita simpan di variable
-print("====================================")
-print("nama \t: {}".format(nama))
-print("nim \t: {}".format(nim))
-print("prodi \t: {}".format(prodi))
-# print("nama \t: {} \nnim \t: {} \nprodi \t: {}".format(nama,nim,prodi))
+print(full_name)
---
+++ pertemuan2.py
@@ -0,0 +1,11 @@
+
+nama = 'aufal marom'
+nim = 2134
+
+
+
+
+nama = 'bihaq'
+nim = 2305
+print("nama saya ", nama)
+print("nim saya ", nim)
