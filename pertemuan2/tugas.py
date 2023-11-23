709fc4c1246e1fd57e96d9c1d19532dcefc66f8d
aufall02 <aufaluzumaki@gmail.com>
2023-11-23 14:26:23 +0700

    tugas 2

---
+++ pertemuan2/tugas.py
@@ -0,0 +1,43 @@
+"""
+soal 1
+buatlah sebuah variable "nama depan, nama belakang, program studi, nomor induk mahasiswa" dengan menerapkan variable "Snake Case", dan code pada print harus 1 baris
+
+
+Output: 
+Nama Depan: Aufal 
+Nama Belakang: Marom 
+Program Studi: Teknik Informatika 
+NIM: 2134
+"""
+
+nama_depan = "aufal"
+nama_belakang = "marom"
+program_studi = "teknik informatika"
+nomer_induk_mahasiswa = "2134"
+
+print("nama depan : ", nama_depan);print("nama belakang : ",nama_belakang);print("program studi : ",program_studi);print("NIM : ",nomer_induk_mahasiswa)
+
+
+
+"""
+soal 2
+
+ubahlah variable nama, nim dibawah ini dengan nama dan nim kalian masing2 tanpa mengubah nilai dari variable di soal
+code soal :
+nama = "Aufal Marom" 
+nim = "2134" 
+dan tampilkan hasilnya
+
+output : 
+nama : kalian
+nim : kalian
+"""
+
+print("===================================================================")
+
+nama = "Aufal marom"
+nim = "2134"
+nama = "bilhaq"
+nim = "2305"
+
+print("nama : {} \nNim : {}".format(nama,nim))
