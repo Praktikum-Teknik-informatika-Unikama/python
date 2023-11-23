97d7785f488e00dd13c7cff64c1c4eca733fc852
aufall02 <aufaluzumaki@gmail.com>
2023-11-23 13:45:19 +0700

    .

--- pertemuan 3/main.py
+++ pertemuan 3/main.py
@@ -1,59 +1,21 @@
-# Operator matematika di phyton
+" Operator aritmatika "
+"Arit matika"
 
-# variable yang akan kita gunakan
-x = 5
-y = 7
+angka_pertama = 3
+angka_kedua = 4
+hasil = angka_kedua ** angka_pertama
 
-# penjumlahan
-# hasil_jumlah = x + y
-# # pengurungan
-# hasil_jumlah = x - y
-# # perkalian
-# hasil_jumlah = x * y
-# # pembagian
-# hasil_jumlah = x / y
-# # modulus (sisa bagi)
-# hasil_jumlah = y % x
-# # Eksponensial (hasil bagi tanpa koma))
-# hasil_jumlah = y ** x
-# Floor division
-hasil_jumlah = y // x
+
 
 "Operator penugasan"
-hasil_jumlah_penugasan = 6
-# hasil_jumlah_penugasan += 7
-# hasil_jumlah_penugasan -= 7
-# hasil_jumlah_penugasan *= 7
-# hasil_jumlah_penugasan /= 7
-# hasil_jumlah_penugasan %= 7
-# hasil_jumlah_penugasan //= 7
-# hasil_jumlah_penugasan **= 7
-hasil_jumlah_penugasan &= 7
-hasil_jumlah_penugasan |= 7
-hasil_jumlah_penugasan ^= 7
+hasil = angka_pertama
+hasil *= angka_kedua
+"hasil = hasil * angka_kedua"
 
 
-"Operator perbandingan"
+"Operator penugasan"
+angka_pertama = 4
+angka_kedua = 6
+hasil = angka_pertama > angka_kedua == angka_pertama == angka_kedua
 
-hasil_jumlah_perbandingan = (x == y ) #sama dengan
-hasil_jumlah_perbandingan = (x != y ) #tidak sama dengan
-hasil_jumlah_perbandingan = (x > y ) #lebih besar dari
-hasil_jumlah_perbandingan = (x < y ) #lebih kecil dari
-hasil_jumlah_perbandingan = (x >= y ) #lebih besar dari atau sama dengan
-hasil_jumlah_perbandingan = (x <= y ) #lebih kecil dari atau sama dengan
-
-
-"Operator Logika"
-
-hasil_jumlah_logika = x and y
-
-
-
-
-
-
-
-# print("hasil : ",hasil_jumlah)
-print("hasil : ",hasil_jumlah_penugasan)
-# print("hasil : ",hasil_jumlah_perbandingan)
-# print("hasil : ",hasil_jumlah_logika)
+print(hasil)
--- pertemuan 3/tugas.py
+++ pertemuan 3/tugas.py
@@ -1,21 +1,13 @@
 "230403010005"
 
 "panjang alas di ambil dari kode angkatan di nim"
-a = 23
+alas = 23
 
 "ukuran tinggi di ambil dari kode unik angka dari nim (kalo depannya 0 cukup masukkan angka terakhir)"
-t = 5
+tinggi = 5
 
 "hitung luas segitiga"
-l_segitiga = 1/2 * a * t
-
 "tampikan luas segita"
-print(l_segitiga)
-
 "ubah hasilnya integer"
-l_segitiga = int(l_segitiga)
+"cek apakah bilangan genap"
 
-"cek apakah bilangan genap atau ganjil"
-l_segitiga = l_segitiga % 2 == 0
-
-print(l_segitiga)
