6717f261023d694def54cfc024acbb9de78e7dc3
aufall02 <aufaluzumaki@gmail.com>
2023-12-21 13:32:30 +0700

    .

--- pertemuan 6/main.py
+++ pertemuan 6/main.py
@@ -1,5 +1,30 @@
-def hello(string): 
-    print('hallo word', string)
+# def sapa(nama):
+#     print('hallo', nama)
 
+# def sum(a,b) :
+#     print("hasil :", a+b)
 
-hello("anjay")
+# def menyapa(*orang) : 
+#     return 'hallo '+ orang[-1]
+    
+# def ujicoba() :
+#     print("ini adalah uji coba")
+
+# def mau_mencoba(satu, dua):
+#     A=satu+dua
+#     print("A adalah=",A)
+
+# # sapa("febri")
+# # sum(3,5)
+# # print(menyapa("bili", "febri", "bagus", "vira"))
+# # ujicoba()
+# mau_mencoba(2,5)
+
+def piramid(angka) :
+    for i in range(angka+1) :
+        for a in range(i) :
+            print('*', end = '')
+
+        print('')
+
+piramid(5)
