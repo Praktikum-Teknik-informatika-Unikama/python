7e8270cedc5423a74a3121513f2520ace790210b
aufall02 <aufaluzumaki@gmail.com>
2023-12-21 14:33:50 +0700

    .

--- pertemuan 6/main.py
+++ pertemuan 6/main.py
@@ -1,24 +1,24 @@
-# def sapa(nama):
-#     print('hallo', nama)
+def sapa(nama):
+    print('hallo', nama)
 
-# def sum(a,b) :
-#     print("hasil :", a+b)
+def sum(a,b) :
+    print("hasil :", a+b)
 
-# def menyapa(*orang) : 
-#     return 'hallo '+ orang[-1]
+def menyapa(*orang) : 
+    return 'hallo '+ orang[-1]
     
-# def ujicoba() :
-#     print("ini adalah uji coba")
+def ujicoba() :
+    print("ini adalah uji coba")
 
-# def mau_mencoba(satu, dua):
-#     A=satu+dua
-#     print("A adalah=",A)
+def mau_mencoba(satu, dua):
+    A=satu+dua
+    print("A adalah=",A)
 
-# # sapa("febri")
-# # sum(3,5)
-# # print(menyapa("bili", "febri", "bagus", "vira"))
-# # ujicoba()
-# mau_mencoba(2,5)
+sapa("febri")
+sum(3,5)
+print(menyapa("bili", "febri", "bagus", "vira"))
+ujicoba()
+mau_mencoba(2,5)
 
 def piramid(angka) :
     for i in range(angka+1) :
